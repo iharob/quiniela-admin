1ad527ab597742a1bac1efd3b3a5bdd38435bf36
Iharob Al Asimi <iharob@gmail.com>
2026-06-24 14:38:48 -0400

    feat: add Resultados (PDF) admin page (#1)
    New Resultados tab showing the real-results PDF from GET /admin/results/pdf inline, with Actualizar and Descargar.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { ResetPasswordPage } from './pages/ResetPasswordPage'
 import { UsersPage } from './pages/UsersPage'
 import { UserDetailPage } from './pages/UserDetailPage'
 import { GamesPage } from './pages/GamesPage'
+import { ResultsPdfPage } from './pages/ResultsPdfPage'
 import { PaymentsPage } from './pages/PaymentsPage'
 import { PaymentMethodsPage } from './pages/PaymentMethodsPage'
 
@@ -28,6 +29,7 @@ export function App(): JSX.Element {
         <Route path="/users" element={<UsersPage />} />
         <Route path="/users/:userId" element={<UserDetailPage />} />
         <Route path="/games" element={<GamesPage />} />
+        <Route path="/results" element={<ResultsPdfPage />} />
         <Route path="/payments" element={<PaymentsPage />} />
         <Route path="/payment-methods" element={<PaymentMethodsPage />} />
       </Route>
